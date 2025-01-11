03d8e773fed26b7b37ebdd802bbcec792e88c3b3
peter <petergrossmann@email.de>
2025-01-11 15:40:31 +0100

    add Pool and Sample pages, update layout with navigation links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/", // Incoming request path
+        destination: "/mint", // Path to serve content from
+      },
+    ];
+  },
 };
 
 export default nextConfig;
